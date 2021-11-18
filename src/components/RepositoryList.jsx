7cc7e7fc7c5f037581f56435be9c1722e15b57b3
= <=>
2021-11-18 20:28:35 -0300

    Utilizando UseEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 const repository = {
@@ -7,6 +8,14 @@ const repository = {
 };
 
 export function RepositoryList(){
+  const [repositeries, setRepositeries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(response => response.json())   
+    .then(data => setRepositeries(data))
+  }, []);
+
   return (
    <section className="repository-list">
      <h1>Lista de repositórios</h1>
