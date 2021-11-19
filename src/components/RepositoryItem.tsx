4be6bc6215e6d072fb6ca820060cc1ea56112708
= <=>
2021-11-18 22:26:44 -0300

    Configurando TypeScript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,3 +1,11 @@
+interface RepositoryItemProps {
+  repository : {
+    name : String;
+    description: String;
+    html_url: String;
+  }
+}
+
 export function RepositoryItem(props){
   return (
     <li>
