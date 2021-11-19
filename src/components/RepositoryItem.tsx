34d49ccbed000dc7fdf2ecb0e85eae13a1e2ff3b
= <=>
2021-11-18 22:47:33 -0300

    Componentes com TypeScript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,17 +1,19 @@
 interface RepositoryItemProps {
   repository : {
-    name : String;
-    description: String;
-    html_url: String;
+    name : string;
+    description: string;
+    html_url: string;
   }
 }
 
-export function RepositoryItem(props){
+export function RepositoryItem(props : RepositoryItemProps){
   return (
     <li>
-      <strong>{ props.repository?.name ?? 'Default' }</strong>
-      <p>{ props.repository?.description }</p>
-      <a href={ props.repository?.html_url }>Acessar repositório</a>
+      <strong>{ props.repository.name ?? 'Default' }</strong>
+      <p>{ props.repository.description }</p>
+      <a href={props.repository.html_url}>
+        Acessar repositório
+      </a>
     </li>
   );
 }
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,8 +1,16 @@
 import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss';
+
+interface Repository{
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList(){
-  const [repositories, setRepositeries] = useState([]);
+  const [repositories, setRepositeries] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
